97b6a1b2e78ea9c42f891b0bcc0935c104cd3963
kaushal328 <84721031+kaushal328@users.noreply.github.com>
2024-08-09 10:56:02 +0530

    Update edit.blade.php
    09/08/24

--- university-students-crud/university-students-crud/resources/views/students/edit.blade.php
+++ university-students-crud/university-students-crud/resources/views/students/edit.blade.php
@@ -15,12 +15,12 @@
 
                         <div class="mb-4">
                             <label for="student_name" class="block text-sm font-medium text-gray-700">Student Name</label>
-                            <input type="text" name="student_name" id="student_name" value="{{ $student->student_name }}" class="mt-1 block w-full" required>
+                            <input type="text" name="student_name" id="student_name" value="{{ $student->student_name }}" class="mt-1 block w-full" required @error('student_name') is-invalid @enderror>
                         </div>
 
                         <div class="mb-4">
                             <label for="teacher_id" class="block text-sm font-medium text-gray-700">Class Teacher</label>
-                            <select name="teacher_id" id="teacher_id" class="mt-1 block w-full" required>
+                            <select name="teacher_id" id="teacher_id" class="mt-1 block w-full" required @error('teacher_id') is-invalid @enderror>
                                 @foreach($teachers as $teacher)
                                     <option value="{{ $teacher->id }}" {{ $student->teacher_id == $teacher->id ? 'selected' : '' }}>
                                         {{ $teacher->name }}
@@ -31,12 +31,12 @@
 
                         <div class="mb-4">
                             <label for="class" class="block text-sm font-medium text-gray-700">Class</label>
-                            <input type="text" name="class" id="class" value="{{ $student->class }}" class="mt-1 block w-full" required>
+                            <input type="text" name="class" id="class" value="{{ $student->class }}" class="mt-1 block w-full" required @error('class') is-invalid @enderror>
                         </div>
 
                         <div class="mb-4">
                             <label for="admission_date" class="block text-sm font-medium text-gray-700">Admission Date</label>
-                            <input type="date" name="admission_date" id="admission_date" value="{{  $student->admission_date }}" class="mt-1 block w-full" required>
+                            <input type="date" name="admission_date" id="admission_date" value="{{  $student->admission_date }}" class="mt-1 block w-full" required >
                         </div>
 
                         <div class="mb-4">
